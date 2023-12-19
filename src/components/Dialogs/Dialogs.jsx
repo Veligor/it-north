795b7a554591caea9034446ce166b3839a415823
Veligor <spaunroom@gmail.com>
2023-12-19 15:54:29 +0300

    new-dialog

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,24 @@
 import React from "react";
-import s from "./Dialog.module.css"
+import s from "./Dialog.module.css";
 
 const Dialogs = (props) => {
-   return <div>
-      <h1>Dialogs</h1>
-   </div>
-}
+  return (
+    <div className={s.dialogs}>
+      <div className={s.human}>
+        <div className={`${s.dialog} ${s.active}`}>Vladimir</div>
+        <div className={s.dialog}>Mariya</div>
+        <div className={s.dialog}>Savva</div>
+        <div className={s.dialog}>Varvara</div>
+        <div className={s.dialog}>Nikolai</div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Hi</div>
+        <div className={s.message}>Hi are you</div>
+        <div className={s.message}>Welcome</div>
+        <div className={s.message}>Forever together</div>
+        <div className={s.message}>leader</div>
+      </div>
+    </div>
+  );
+};
 export default Dialogs;
