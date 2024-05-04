227d957939fb32b395c40bb0478b6302d41f3f31
Veligor <spaunroom@gmail.com>
2024-05-04 18:52:08 +0300

    lazy

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import "./App.css";
 import React from "react";
 import HeaderContainer from "./components/Header/HeaderContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
 import { Route, Routes } from "react-router-dom";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
+import { lazy, Suspense } from 'react';
 import NavbarContainer from "./components/Navbar/NavbarContainer";
 import UsersContainer from "./components/Users/usersContainer";
 import News from "./components/News/News"
@@ -12,6 +11,11 @@ import { useEffect } from "react";
 import {initializeApp} from "./components/Redux/app-reducer"
 import { useDispatch, useSelector } from "react-redux";
 import Preloader from "./components/hhh/Preloader";
+
+
+const DialogsContainer = lazy(() => import('./components/Dialogs/DialogsContainer'));
+const ProfileContainer = lazy(() => import('./components/Profile/ProfileContainer'));
+
 
 function App(props) {
 
@@ -30,17 +34,20 @@ if(!iditialiazed) {
       <HeaderContainer />
       <NavbarContainer />
       <div className="app-wrapper-content">
+      <Suspense fallback={<div><Preloader /></div>}>
         <Routes>
         <Route path="/profile" element={<ProfileContainer />} />
         <Route path="/profile/:profileId" element={<ProfileContainer />} />
-        <Route path="/dialogs" element={<DialogsContainer />} />
+        <Route path="/dialogs" element={ <DialogsContainer />} />
         <Route path="/users" element={<UsersContainer />} />
         <Route path="/login" element={<Login />} />
         <Route path="/news" element={<News />} />
         </Routes>
+        </Suspense>
       </div>
     </div> 
   );
 }
 
 export default App;
+
