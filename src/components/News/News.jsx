f401d94a2227d3ee56084af0dfd758427d52ade8
VeligorMan <spaunroom@gmail.com>
2024-06-24 17:23:05 +0300

    change news

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,22 +1,45 @@
 import React, {useState} from "react";
 
 
-export const News = () => {
+export default function News({
+  status = 'error'
+}) {
+  // const [score, setScore] = useState(0);
+  // // функция обновления 
+  // function increment() {
+  //   setScore(s => s + 1);
+  // }
+  // // setScore(score + 1);
+  // return (
+  //   <>
+  //     <button onClick={() => increment()}>+1</button>
+  //     <button onClick={() => {
+  //       increment();
+  //       increment();
+  //       increment();
+  //     }}>+3</button>
+  //     <h1>Score: {score}</h1>
+  //   </>
+  // )
   
+  if (status === 'success') {
+    return <h1>That's right!</h1>
+  }
+  return (
+    <>
+      <h2>City quiz</h2>
+      <p>
+        In which city is there a billboard that turns air into drinkable water?
+      </p>
+      <form>
+        <textarea />
+        <br />
+        <button>
+          Submit
+        </button>
+      </form>
+    </>
+  )
   
-  return <div>
-    <div>
-      <input placeholder="search" /><button>find</button>
-    </div>
-    <ul>
-      {['Vovan', 'Savva'].map(n => <li>{n}</li>)}
-    </ul>
-    <div>
-      <h2>Username</h2>
-      <div>Details</div>
-    </div>
-  </div>
-
- } 
-
- export default News;
+  }
+  
